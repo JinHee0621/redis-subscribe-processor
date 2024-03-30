f567c8097fdf317075a52bd196f561cd82a23290
Jinhee Han <lsd0017@naver.com>
2024-03-31 00:55:50 +0900

    Fix Source

--- nifi-custom-processors/src/main/java/com/subscribe/nifi/RedisSubscribe.java
+++ nifi-custom-processors/src/main/java/com/subscribe/nifi/RedisSubscribe.java
@@ -17,6 +17,7 @@
 package com.subscribe.nifi;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.nifi.annotation.lifecycle.OnStopped;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.annotation.behavior.ReadsAttribute;
@@ -27,12 +28,8 @@ import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.SeeAlso;
 import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.processor.*;
 import org.apache.nifi.processor.exception.ProcessException;
-import org.apache.nifi.processor.AbstractProcessor;
-import org.apache.nifi.processor.ProcessContext;
-import org.apache.nifi.processor.ProcessSession;
-import org.apache.nifi.processor.ProcessorInitializationContext;
-import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.io.StreamCallback;
 import org.apache.nifi.processor.util.StandardValidators;
 
@@ -41,11 +38,13 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisPubSub;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Tags({"Redis", "PubSub", "Subscribe"})
 @CapabilityDescription("Provide a description")
@@ -125,14 +124,14 @@ public class RedisSubscribe extends AbstractProcessor {
     public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
         return descriptors;
     }
-
-    @OnScheduled
-    public void onScheduled(final ProcessContext context) {
-
-    }
     Jedis subscriberJedis = null;
     JedisPool jedisPool = null;
     JedisPoolConfig poolConfig = new JedisPoolConfig();
+    ExecutorService subscribers;
+    RedisRes subscriber = null;
+    @OnScheduled()
+    public void onScheduled(final ProcessContext context) {
+    }
     @Override
     public void onTrigger(final ProcessContext context, final ProcessSession session) {
         try {
@@ -140,14 +139,34 @@ public class RedisSubscribe extends AbstractProcessor {
             int redisPort = Integer.parseInt(context.getProperty(PORT).getValue());
             String channelNm = context.getProperty(CHANNEL).getValue();
             String password = context.getProperty(PASSWORD).getValue();
+
             if(password != null) {
-                jedisPool = new JedisPool(poolConfig, redisHost, redisPort, 0, password);
+                jedisPool = new JedisPool(poolConfig, redisHost, redisPort, 1000, password);
             } else {
-                jedisPool = new JedisPool(poolConfig, redisHost, redisPort, 0);
+                jedisPool = new JedisPool(poolConfig, redisHost, redisPort, 1000);
             }
+
             subscriberJedis = jedisPool.getResource();
-            RedisRes subscriber = new RedisRes(subscriberJedis,"onlyOne", session, REL_SUCCESS);
-            subscriberJedis.subscribe(subscriber, channelNm);
+            subscribers = Executors.newSingleThreadExecutor();
+            subscriber = new RedisRes(subscriberJedis, session, REL_SUCCESS, subscribers);
+
+            subscribers.submit(() -> {
+                        subscriberJedis.subscribe(subscriber, channelNm);
+                }
+            );
+
+            subscribers.awaitTermination(5000, TimeUnit.MILLISECONDS);
+
+            if(subscribers.isShutdown()) {
+                subscribers.shutdownNow();
+                subscribers = null;
+            }
+
+            if(subscriberJedis.isConnected()) {
+                subscriberJedis.disconnect();
+                subscriberJedis.close();
+                subscriberJedis = null;
+            }
 
         } catch (Exception e) {
             FlowFile flowFile = session.create();
@@ -161,34 +180,52 @@ public class RedisSubscribe extends AbstractProcessor {
             if(subscriberJedis != null) subscriberJedis.close();
         }
     }
-    class RedisRes extends JedisPubSub {
-        private Jedis subscriber;
-        private String name;
-        private ProcessSession session;
-        private Relationship REL_SUCCESS;
-        public RedisRes(Jedis subscriber, String name, final ProcessSession session, Relationship REL_SUCCESS) {
-            this.subscriber = subscriber;
-            this.name = name;
-            this.session = session;
-            this.REL_SUCCESS = REL_SUCCESS;
-        }
-        @Override
-        public void onMessage(String channel, String message) {
-            FlowFile flowFile = session.create();
-            final String output = "Messsage : " + message ;//"name:"+ name + "method:" + "onUnsubscribe" + "channel: "+channel+" subscribedChannels: %d\n";
-            flowFile = session.write(flowFile, new StreamCallback() {
-                @Override
-                public void process(InputStream in, OutputStream outputStream) throws IOException {
-                    IOUtils.write(output, outputStream, "UTF-8");
-                }
-            });
-            session.transfer(flowFile, REL_SUCCESS);
-            // Redis Client Close
-            this.unsubscribe();
-            jedisPool.returnResource(subscriber);
-            subscriber.close();
+    @OnStopped
+    public void stopSubscription(final ProcessContext context) {
+        if (subscriber != null && subscriber.isSubscribed()) {
+            try {
+                subscriber.unsubscribe();
+                subscribers.awaitTermination(5000, TimeUnit.MILLISECONDS);
+                subscribers.shutdownNow();
+                subscribers = null;
+            } catch (InterruptedException e) {
+                getLogger().warn("Unable to cleanly shutdown due to {}", new Object[]{e});
+            }
         }
     }
 }
+
+class RedisRes extends JedisPubSub {
+    private Jedis subscriber;
+    private ProcessSession session;
+    private Relationship REL_SUCCESS;
+    private ExecutorService executor;
+    public RedisRes(Jedis subscriber,final ProcessSession session, Relationship REL_SUCCESS, ExecutorService executor) {
+        this.subscriber = subscriber;
+        this.session = session;
+        this.REL_SUCCESS = REL_SUCCESS;
+        this.executor = executor;
+    }
+    @Override
+    public void onMessage(String channel, String message) {
+        FlowFile flowFile = session.create();
+        final String output = "Messsage : " + message ;//"name:"+ name + "method:" + "onUnsubscribe" + "channel: "+channel+" subscribedChannels: %d\n";
+        flowFile = session.write(flowFile, new StreamCallback() {
+            @Override
+            public void process(InputStream in, OutputStream outputStream) throws IOException {
+                IOUtils.write(output, outputStream, "UTF-8");
+            }
+        });
+        session.transfer(flowFile, REL_SUCCESS);
+        this.unsubscribe();
+
+        // Redis Client Close
+        subscriber.disconnect();
+        subscriber.close();
+        executor.shutdownNow();
+    }
+}
+
+
 
 
